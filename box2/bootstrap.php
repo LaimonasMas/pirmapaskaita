ca7bb3b49fd6eb0a325c261e79c0c39416066f30
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-15 12:50:15 +0200

    update isskirtos funkcijos

---
+++ box2/bootstrap.php
@@ -0,0 +1,21 @@
+
+<?php
+session_start();
+define('URL', 'http://localhost/zuikis/box2/'); // <--- konstanta
+define('DIR', __DIR__.'/');
+
+define('INSTALL_DIR', '/zuikis/box2/');
+
+
+require DIR. 'app/BananaController.php';
+require DIR. 'app/Json.php';
+require DIR. 'app/Box.php';
+require DIR. 'app/Helper.php';
+
+
+
+
+// _d($_SESSION, 'SESIJA--->');
+
+// http://localhost/zuikis/box/
+
