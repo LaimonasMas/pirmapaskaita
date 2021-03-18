b17f8ac322152ae9b0fd7f63f2ee1fa4202fa4a2
Laimonas Masionis <71811684+LaimonasMas@users.noreply.github.com>
2021-03-18 23:03:14 +0200

    currency with cache

--- api/index.php
+++ api/index.php
@@ -1,60 +1,60 @@
 <?php
 $dist = null;
-define('API', 'https://www.distance24.org/route.json?stops=');
+define('API','https://www.distance24.org/route.json?stops=');
 
 if (!empty($_GET)) {
+
+// if (!file_exists(__DIR__.'/data.json')) {
+//     $empty = ['time' => time(), 'data' => []]
+// }
+
 
 
 
 $city1 = $_GET['c1'];
 $city2 = $_GET['c2'];
-
 // serverio vidus
+
 $curl = curl_init();
 curl_setopt($curl, CURLOPT_URL, API.$city1.'|'.$city2);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
-
-$answer = curl_exec($curl); // siunciame uzklausa, tada laukiame...... atsakyma irasome i $answer
-
+$answer = curl_exec($curl); // siunciame uzklausa. laukiame........ atsakyma irasome i $answer
 curl_close($curl);
-
 $answer = json_decode($answer);
-
-_d($answer); 
-_d($answer->stops[0]->wikipedia); 
-
-
 $dist = $answer->distance;
-
 $image1 = $answer->stops[0]->wikipedia->image ?? '';
 $image2 = $answer->stops[1]->wikipedia->image ?? '';
+$result = 'API';
+
 }
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=h2, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>API DISTANCE-24</title>
 </head>
-
 <body>
-
 <form action="" method="get">
+Miestas 1 <input type="text" name="c1">
 
-Miestas 1 <input type="text" name="c1" id="">
-Miestas 2 <input type="text" name="c2" id="">
+Miestas 2 <input type="text" name="c2">
+
 <button type="submit">Atstumas</button>
-
 </form>
-<?php if (null != $dist) : ?>
-    <h2>Atstumas <?= $dist ?> km</h2>
-    <img style="width:400px" src="<?= $answer->stops[0]->wikipedia->image ?>" alt="">
-    <img style="width:400px" src="<?= $answer->stops[1]->wikipedia->image ?>" alt="">
-<?php endif ?>
-</body>
 
+<?php if(null !== $dist) : ?>
+<h1 style="color:red">Result from: <?= $result ?> </h1>
+<h2>Atstumas: <?= $dist ?> km</h2>
+<img style="height: 400px;" src="<?= $image1 ?>">
+<img style="height: 400px;" src="<?= $image2 ?>">
+<?php endif ?>
+
+    
+</body>
 </html>
